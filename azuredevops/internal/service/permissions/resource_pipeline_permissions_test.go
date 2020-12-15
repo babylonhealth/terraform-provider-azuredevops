adf663cdd7cc1de8fe57e605ae033ce21f2dccdd
Nitesh Lall <46679212+nitesh-babylon@users.noreply.github.com>
2020-12-15 13:59:39 +0000

    [DEVP-1851] set permissions per pipeline (#12)
    * [DEVP-1851] successfully set permissions
    
    * add support to set for all pipelines in a project
    
    * correct token with tests
    
    * go fmt
    
    * remove test using direct azure interaction
    
    * remove unused code
    
    * fix provider test
    
    * update provider
    
    * add examples
    
    (cherry picked from commit 616919486607141c24865860392d25fe024e9a09)

---
+++ azuredevops/internal/service/permissions/resource_pipeline_permissions_test.go
@@ -0,0 +1,94 @@
+package permissions
+
+// The tests in this file use the mock clients in mock_client.go to mock out
+// the Azure DevOps client operations.
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+/**
+ * Begin unit tests
+ */
+
+func TestBuildPermissions_CreateBuildToken(t *testing.T) {
+	projectID := "9083e944-8e9e-405e-960a-c80180aa71e6"
+	buildID := "29"
+	expectedToken := fmt.Sprintf("%s/%s", projectID, buildID)
+
+	d := getBuildPermissionsResource(t, projectID, buildID, false)
+	gotToken, err := createBuildToken(d, nil)
+	assert.NotEmpty(t, gotToken)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedToken, gotToken)
+
+	expectedErr := fmt.Errorf("failed to get 'project_id' from schema")
+	d = getBuildPermissionsResource(t, "", "", false)
+	token, err := createBuildToken(d, nil)
+	assert.Empty(t, token)
+	assert.Equal(t, expectedErr, err)
+
+	expectedToken = fmt.Sprintf("%s", projectID)
+	d = getBuildPermissionsResource(t, projectID, "", true)
+	err = d.Set("build_id", nil)
+	token, err = createBuildToken(d, nil)
+	assert.NotEmpty(t, gotToken)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedToken, token)
+
+	expectedErr = fmt.Errorf("build_id cannot be set when project_level is true")
+	d = getBuildPermissionsResource(t, projectID, "1234", true)
+	token, err = createBuildToken(d, nil)
+	assert.Empty(t, token)
+	assert.Equal(t, expectedErr, err)
+
+	expectedErr = fmt.Errorf("build_id required when project_level is not true")
+	d = getBuildPermissionsResource(t, projectID, "", false)
+	token, err = createBuildToken(d, nil)
+	assert.Empty(t, token)
+	assert.Equal(t, expectedErr, err)
+
+	expectedErr = fmt.Errorf("build_id required when project_level is not true")
+	d = getBuildPermissionsResource(t, projectID, "1234", false)
+	err = d.Set("build_id", nil)
+	token, err = createBuildToken(d, nil)
+	assert.Empty(t, token)
+	assert.Equal(t, expectedErr, err)
+}
+
+func getBuildPermissionsResource(t *testing.T, projectID string, buildID string, projectLevel bool) *schema.ResourceData {
+	d := schema.TestResourceDataRaw(t, ResourcePipelinePermissions().Schema, nil)
+	if projectID != "" {
+		d.Set("project_id", projectID)
+	}
+	if buildID != "" {
+		d.Set("build_id", buildID)
+	}
+
+	d.Set("project_level", projectLevel)
+
+	d.Set("principal", "vssgp.Uy0xLTktMTU1MTM3NDI0NS0yNDU1NjAwOTc1LTI4NjEzNDU5NS0yODk0NDM2NzIyLTIyNTMwNDg3NzAtMS0zNzc2MDEyNDM3LTI1MjAyNTQ3OTAtMjYxOTIwMDAzOS0yNTg5OTY1NzE4")
+
+	d.Set("permissions", map[string]string{
+		"ViewBuilds":                     "allow",
+		"EditBuildQuality":               "allow",
+		"RetainIndefinitely":             "allow",
+		"DeleteBuilds":                   "allow",
+		"ManageBuildQualities":           "allow",
+		"DestroyBuilds":                  "allow",
+		"UpdateBuildInformation":         "allow",
+		"QueueBuilds":                    "allow",
+		"ManageBuildQueue":               "allow",
+		"StopBuilds":                     "allow",
+		"ViewBuildDefinition":            "allow",
+		"EditBuildDefinition":            "allow",
+		"DeleteBuildDefinition":          "allow",
+		"OverrideBuildCheckInValidation": "allow",
+		"AdministerBuildPermissions":     "allow",
+	})
+
+	return d
+}
--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ locals {
 }
 
 provider "azuredevops" {
+  version = "= 0.0.4"
   org_service_url = "https://dev.azure.com/${local.azure_org}/"
 }
 
@@ -16,8 +17,57 @@ resource "azuredevops_serviceendpoint_github" "gh" {
 
 }
 
-resource "azuredevops_gitapp" "app" {
+resource "azuredevops_serviceendpoint_githubapp" "app" {
   project_id = azuredevops_project.project.id
   connection_id = azuredevops_serviceendpoint_github.gh.id
   repo = "babylonhealth/pipeline-historian"
+}
+
+resource "azuredevops_build_permissions" "admin_permission3" {
+    project_id      = "4f7f5d92-0e11-4311-ac85-9972864acbc2"
+    principal = "vssgp.Uy0xLTktMTU1MTM3NDI0NS0yNDU1NjAwOTc1LTI4NjEzNDU5NS0yODk0NDM2NzIyLTIyNTMwNDg3NzAtMS0zNzc2MDEyNDM3LTI1MjAyNTQ3OTAtMjYxOTIwMDAzOS0yNTg5OTY1NzE4"
+    project_level = true
+    replace         = true
+    permissions = {
+        ViewBuilds                          = "allow"
+        EditBuildQuality                    = "allow"
+        RetainIndefinitely                  = "allow"
+        DeleteBuilds                        = "allow"
+        ManageBuildQualities                = "allow"
+        DestroyBuilds                       = "allow"
+        UpdateBuildInformation              = "allow"
+        QueueBuilds                         = "allow"
+        ManageBuildQueue                    = "allow"
+        StopBuilds                          = "allow"
+        ViewBuildDefinition                 = "allow"
+        EditBuildDefinition                 = "allow"
+        DeleteBuildDefinition               = "allow"
+        OverrideBuildCheckInValidation      = "allow"
+        AdministerBuildPermissions          = "deny"
+    }
+}
+
+resource "azuredevops_build_permissions" "admin_permission4" {
+    project_id      = "4f7f5d92-0e11-4311-ac85-9972864acbc2"
+    principal = "vssgp.Uy0xLTktMTU1MTM3NDI0NS0yNDU1NjAwOTc1LTI4NjEzNDU5NS0yODk0NDM2NzIyLTIyNTMwNDg3NzAtMS0zNzc2MDEyNDM3LTI1MjAyNTQ3OTAtMjYxOTIwMDAzOS0yNTg5OTY1NzE4"
+    project_level = false
+    build_id = 39
+    replace         = true
+    permissions = {
+        ViewBuilds                          = "allow"
+        EditBuildQuality                    = "allow"
+        RetainIndefinitely                  = "allow"
+        DeleteBuilds                        = "allow"
+        ManageBuildQualities                = "allow"
+        DestroyBuilds                       = "allow"
+        UpdateBuildInformation              = "allow"
+        QueueBuilds                         = "allow"
+        ManageBuildQueue                    = "allow"
+        StopBuilds                          = "allow"
+        ViewBuildDefinition                 = "allow"
+        EditBuildDefinition                 = "allow"
+        DeleteBuildDefinition               = "allow"
+        OverrideBuildCheckInValidation      = "allow"
+        AdministerBuildPermissions          = "deny"
+    }
 }
