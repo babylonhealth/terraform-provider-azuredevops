0d3cc2aa04c80a5b0b079d00e26d69c8b73f2765
Luke Carrier <luke.carrier@babylonhealth.com>
2023-07-13 16:16:52 +0100

    Introduce debugging support

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"flag"
 	"github.com/babylonhealth/terraform-provider-bblnazuredevops/bblnazuredevops"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debug,
 		ProviderAddr: "babylonhealth.com/babylonhealth/bblnazuredevops",
 		ProviderFunc: func() *schema.Provider {
 			return bblnazuredevops.Provider()
 		},
-	})
+	}
+	plugin.Serve(opts)
 }
